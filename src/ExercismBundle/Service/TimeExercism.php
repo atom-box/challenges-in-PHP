51d040b8ac80989e296f71270b5461f49a7f106a
atom-box <atom_box@hotmail.com>
2021-03-19 06:15:31 -0500

    Added clever parse/implode to get dateInterval from raw seconds.

--- src/ExercismBundle/Service/TimeExercism.php
+++ src/ExercismBundle/Service/TimeExercism.php
@@ -14,10 +14,32 @@ class  TimeExercism
 
     public function from(DateTimeImmutable $dateObject)
     {
-        $timeStep = new DateInterval("PT1000000000S");
-        // $timeStep['s'] = 1000000000;
+        // $timeStep = new DateInterval("PT1000000000S");
+        $theGiga = 1000000000000;
+        $timeStep = $this->time_elapsed($theGiga);
+        var_dump($timeStep);
+        die;
+
+
         $dateObject->add($timeStep);
         return $dateObject;
+    }
+    // wow this part is cool; copied it from somewhere!
+    function time_elapsed($secs)
+    {
+        $bit = array(
+            'y' => $secs / 31556926 % 12,
+            'w' => $secs / 604800 % 52,
+            'd' => $secs / 86400 % 7,
+            'h' => $secs / 3600 % 24,
+            'm' => $secs / 60 % 60,
+            's' => $secs % 60
+        );
+
+        foreach ($bit as $k => $v)
+            if ($v > 0) $ret[] = $v . $k;
+
+        return join(' ', $ret);
     }
 }
 
