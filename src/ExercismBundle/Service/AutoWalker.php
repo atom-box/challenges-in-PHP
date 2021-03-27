7fc6f4bac7127566cf4cd12ca06c8926947b4c5a
atom-box <atom_box@hotmail.com>
2021-03-26 21:17:27 -0500

    Start walking robots feature.

---
+++ src/ExercismBundle/Service/AutoWalker.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ExercismBundle\Service;
+
+class AutoWalker
+{
+    // public function __construct()
+    // {
+    //     //
+    // }
+
+    // public function turnLeft()
+    // {
+    //     return '...fudge...';
+    // }
+
+    // public function position()
+    // {
+    //     return '...fudge...';
+    // }
+}
