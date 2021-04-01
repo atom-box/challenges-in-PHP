1d0ef04256c9dd2eb18f43fd023aef577e7d4942
atom-box <atom_box@hotmail.com>
2021-03-31 20:32:50 -0500

    Refactor many instances of Robot. To avoid same class name in this folder.

---
+++ src/ExercismBundle/Service/Autowalker.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ExercismBundle\Service;
+
+class Autowalker
+{
+    // public function __construct()
+    // {
+    //     //
+    // }
+
+    // public function turnLeft()
+    // {
+    //     return '...fudge...';
+    // }
+
+    // public function position()
+    // {
+    //     return '...fudge...';
+    // }
+}
