764d27e296a8ab6ee79efd8fbbf07eb7c7418b4e
atom-box <atom_box@hotmail.com>
2021-04-02 21:28:18 -0500

    WIP: implemented clever  array for directions but now constants not working.

--- src/ExercismBundle/Service/Autowalker.php
+++ src/ExercismBundle/Service/Autowalker.php
@@ -5,10 +5,17 @@ namespace ExercismBundle\Service;
 class Autowalker
 {
     const DIRECTION_NORTH = [0, 1];
-    const DIRECTION_SOUTH = [0, -1];
     const DIRECTION_EAST = [1, 0];
+    const DIRECTION_SOUTH = [0, -1];
     const DIRECTION_WEST = [-1, 0];
 
+    // position 0 is always the current, followed by next 3 right turns
+    private $steeringWheel = array(
+        DIRECTION_NORTH,
+        DIRECTION_EAST,
+        DIRECTION_SOUTH,
+        DIRECTION_WEST,
+    );
     public $position = [0, 0];
     public $direction = [0, 1];
 
@@ -18,15 +25,28 @@ class Autowalker
         $this->direction = $direction;
     }
 
+
+
     // public function __construct()
     // {
     //     //
     // }
 
-    // public function turnLeft()
-    // {
-    //     return '...fudge...';
-    // }
+    public function turnLeft()
+    {
+        $temp = array_pop($steeringWheel);
+        array_unshift($steeringWheel, $temp);
+        array_dump($steeringWheel);
+        $direction = $steeringWheel[0];
+    }
+
+    public function turnRight()
+    {
+        $temp = array_push($steeringWheel);
+        array_shift($steeringWheel, $temp);
+        array_dump($steeringWheel);
+        $direction = $steeringWheel[0];
+    }
 
     // public function position()
     // {
